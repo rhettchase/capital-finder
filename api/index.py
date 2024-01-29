bd30f31c7ecbaba5c5646205b57668ab33aae08a
RHETT CHASE <rhettjchase@gmail.com>
2024-01-29 14:02:25 -0800

    separates into get_information function

--- api/index.py
+++ api/index.py
@@ -10,46 +10,34 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
         
-        if "country" in dic:
-            url = f"https://restcountries.com/v3.1/name/{dic['country']}"
-            
-            try:
-                country_req = requests.get(url)
-                country_req.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
-                data = country_req.json()
-                capitals = [country_data["capital"][0] for country_data in data]
-                message = str(capitals)
-            
-            except requests.exceptions.HTTPError as err:
-                message = f"HTTP Error: {err}"
-            except requests.exceptions.RequestException as err:
-                message = f"Request Exception: {err}"
-            except KeyError:
-                message = "Error in parsing country data"
+        message = "Please provide a country or capital name"
         
-        if "capital" in dic:
-            url = f"https://restcountries.com/v3.1/capital/{dic['capital']}"
-            
-            try:
-                capital_req = requests.get(url)
-                capital_req.raise_for_status()
-                data = capital_req.json()
-                countries = [capital_data["name"][0] for capital_data in data]
-                message = str(countries)
-                
-            except requests.exceptions.HTTPError as err:
-                message = f"HTTP Error: {err}"
-            except requests.exceptions.RequestException as err:
-                message = f"Request Exception: {err}"
-            except KeyError:
-                message = "Error in parsing country data"
-            
-        else:
-            message = "Please provide a country or capital name"
+        if "country" in dic:
+            message = self.get_information(f"https://restcountries.com/v3.1/name/{dic['country']}", "capital")
+
+        elif "capital" in dic:
+            message = self.get_information(f"https://restcountries.com/v3.1/capital/{dic['capital']}", "country")
             
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        
         self.wfile.write(message.encode('utf-8'))
         return
+        
+    def get_information(self, url, type):
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+
+            if type == "capital":
+                return str([country_data["capital"][0] for country_data in data])
+            elif type == "country":
+                return str([country_data["name"]["common"] for country_data in data])
+
+        except requests.exceptions.HTTPError as err:
+            return f"HTTP Error: {err}"
+        except requests.exceptions.RequestException as err:
+            return f"Request Exception: {err}"
+        except KeyError:
+            return "Error in parsing data"
