2ca82da202623bc01faef1af7166e85e75f69927
RHETT CHASE <rhettjchase@gmail.com>
2024-01-29 13:47:23 -0800

    add country query to index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,34 @@
 from http.server import BaseHTTPRequestHandler
+from urllib import parse
+import requests
  
-class handler(BaseHTTPRequestHandler): # subclass of BaseHTTPRequestHandler
-    # WRRC
+class handler(BaseHTTPRequestHandler): 
+    
     def do_GET(self):
+        string = self.path
+        url_components = parse.urlsplit(string)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
         
-        message = "Please provide a country or capital name"
+        if "country" in dic:
+            url = f"https://restcountries.com/v3.1/name/{dic['country']}"
+            
+            try:
+                country_req = requests.get(url)
+                country_req.raise_for_status()  # Raises an HTTPError if the HTTP request returned an unsuccessful status code
+                data = country_req.json()
+                countries = [country_data["capital"][0] for country_data in data]
+                message = str(countries)
+            
+            except requests.exceptions.HTTPError as err:
+                message = f"HTTP Error: {err}"
+            except requests.exceptions.RequestException as err:
+                message = f"Request Exception: {err}"
+            except KeyError:
+                message = "Error in parsing country data"
+            
+        else:
+            message = "Please provide a country or capital name"
             
         self.send_response(200)
         self.send_header('Content-type','text/plain')
